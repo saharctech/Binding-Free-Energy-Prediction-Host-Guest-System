159312c8d14faafa1b7894211ff94c42919ff385
Sahar <it_sahar@yahoo.com>
2021-09-30 11:14:38 -0700

    removing pdbbind

--- pdbbind/picked_60_pdbbind/scripts/tleap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for folder in ../picked-60/*; do
-    cd "$folder"
-    mkdir "top_crd"
-    cd "top_crd"
-    $AMBERHOME/bin/tleap -f ../../../scripts/tleap.in
-    cd ../..
-done
--- pdbbind/scripts/tleap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for folder in ../raw_data_test/*; do
-    cd "$folder"
-    mkdir "top_crd"
-    cd "top_crd"
-    $AMBERHOME/bin/tleap -f ../../../scripts/tleap.in
-    cd ../..
-done
